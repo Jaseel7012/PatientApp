6abbbc88ab9f19d4e300570e8d90681df478bac9
jaseel <jaseeljctcs@gmail.com>
2022-12-07 11:36:23 +0530

    patient app initial

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,24 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AddPatientComponent } from './add-patient/add-patient.component';
+import { RouterModule, Routes } from '@angular/router';
+const myroute:Routes=[
+  {
+    path:'',
+    component:AddPatientComponent
+  }
 
+]
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AddPatientComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    RouterModule.forRoot(myroute)
   ],
   providers: [],
   bootstrap: [AppComponent]
